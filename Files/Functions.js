1328ecfc48583a5c4b08ebd7866203cb2b60a410
Jim <51420306+MrYouu@users.noreply.github.com>
2020-06-03 17:32:33 +0300

    Add files via upload

--- Files/Functions.js
+++ Files/Functions.js
@@ -1,1 +1,9 @@
+function openMenu()
+{
+    document.getElementById("sideBar").style.clipPath = "polygon(0 0, 100% 0, 100% 100%, 0 100%)";
+}
 
+function closeMenu()
+{
+    document.getElementById("sideBar").style.clipPath = "polygon(100% 0, 100% 0, 100% 0, 100% 0)";
+}
